dcc91ba823b43171f1aebd842b3cf31720341b88
1Alnew <alnew@iastate.edu>
2025-03-31 18:21:03 -0500

    Some data cleaning

---
+++ Data.R
@@ -0,0 +1,5 @@
+library(readr)
+data <- read_csv("Health_conditions_among_children_under_age_18__by_selected_characteristics__United_States.csv")
+print(unique(data$INDICATOR))
+data <- subset(data, select = -INDICATOR)
+print(unique(data$PANEL))
