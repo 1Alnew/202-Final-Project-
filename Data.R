dd6977e1aa325cc90db48dcfd42c2e29d557c309
1Alnew <alnew@iastate.edu>
2025-04-17 18:25:43 -0500

    added link to documentation

--- Data.R
+++ Data.R
@@ -1,4 +1,5 @@
 library(readr)
 library(tidyverse)
 data <- read_csv("https://raw.githubusercontent.com/1Alnew/202-Final-Project-/refs/heads/main/Crime_Incidents_in_2024.csv")
-
+#link to web page: https://opendata.dc.gov/datasets/DCGIS::crime-incidents-in-2024/about
+view(data)
