d2d6ac27bdecfe5524f32561c69c7f9b891d809c
AshlynnP <ashg1921@iastate.edu>
2025-05-05 20:31:44 -0500

    Second answer

--- Ashlynn.R
+++ Ashlynn.R
@@ -37,9 +37,70 @@ ggplot(top_10_per_shift, aes(x = BLOCK, y = count, fill = SHIFT)) +
   facet_wrap(~SHIFT, scales = "free") +
   scale_x_discrete(labels = function(x) gsub("___.*", "", x)) +
   labs(
-    title = "Top 10 Locations by Crime Count per Shift",
+    title = "Top 10 Locations by Crime Count per Day",
     x = "Location",
     y = "Crime Count"
   ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+top_10_per_shift$BLOCK <- sub("___.*$", "", top_10_per_shift$BLOCK)
+
+
+excluded_locations <- c(
+  "812 - 899 BLOCK OF BLADENSBURG ROAD NE", 
+  "600 - 669 BLOCK OF PENNSYLVANIA AVENUE SE",
+  "1000 - 1099 BLOCK OF 16TH STREET NW",
+  "1516 - 1699 BLOCK OF BENNING ROAD NE",
+  "100 - 199 BLOCK OF CARROLL STREET NW",
+  "3200 - 3275 BLOCK OF M STREET NW",
+  "1600 - 1699 BLOCK OF P STREET NW",
+  "2600 - 2649 BLOCK OF CONNECTICUT AVENUE NW",
+  "6500 - 6599 BLOCK OF GEORGIA AVENUE NW",
+  "1100 - 1199 BLOCK OF U STREET NW",
+  "1300 - 1399 BLOCK OF OKIE STREET NE",
+  "1800 - 2299 BLOCK OF NEW YORK AVENUE NE",
+  "4000 - 4121 BLOCK OF MINNESOTA AVENUE NE",
+  "2100 - 2199 BLOCK OF 24TH PLACE NE",
+  "1 - 7 BLOCK OF DUPONT CIRCLE NW"
+)
+
+filtered_crime <- top_10_per_shift %>%
+  filter(!BLOCK %in% excluded_locations)
+
+ggplot(filtered_crime, aes(x = BLOCK, y = count, fill = SHIFT)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~SHIFT, scales = "free") +
+  scale_x_discrete(labels = function(x) gsub("___.*", "", x)) +
+  labs(
+    title = "Top 10 Locations by Crime Count per Day",
+    x = "Location",
+    y = "Crime Count"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+neighborhood_crimes <- data %>%
+  count(OFFENSE,NEIGHBORHOOD_CLUSTER, name = "count")
+
+neighborhood_crimes <- neighborhood_crimes %>%
+  filter(!is.na(OFFENSE), !is.na(NEIGHBORHOOD_CLUSTER))
+
+
+ggplot(neighborhood_crimes, aes(x = NEIGHBORHOOD_CLUSTER, y = count, fill = OFFENSE)) +
+  geom_col() +
+  labs(
+    title = "Crime Count by Neighborhood Cluster and Offense Type",
+    x = "Neighborhood Cluster",
+    y = "Total Crime Count",
+    fill = "Offense"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "bottom"
+  )
+
+
