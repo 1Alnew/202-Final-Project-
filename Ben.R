1ca73a15527a3c2cf8b4187578744933f4c5ebda
Bhersche <benjamin.ashby.herschel@gmail.com>
2025-05-07 17:36:50 -0500

    don't worry about it

--- Ben.R
+++ Ben.R
@@ -1,5 +1,8 @@
 library(readr)
 library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
 data <- read_csv("https://raw.githubusercontent.com/1Alnew/202-Final-Project-/refs/heads/main/Crime_Incidents_in_2024.csv")
 data <- data %>%  select(-OCTO_RECORD_ID)
 data$DISTRICT <- as.character(data$DISTRICT)
